0b4b49202ab8e13935b309c90b8bd2c6dc3cafac
AlexONEX <a.schwartzmann@hotmail.com>
2023-09-09 15:26:14 -0300

    Entropy+Information by symbol

--- packet_catcher.py
+++ packet_catcher.py
@@ -5,6 +5,7 @@ import math
 
 S1 = {}
 def mostrar_fuente(S):
+    #S must be not empty
     N = sum(S.values())
     simbolos = sorted(S.items(), key=lambda x: -x[1])
 
@@ -16,17 +17,23 @@ def mostrar_fuente(S):
         info = -math.log(probabilidad, 2)
         entropy += probability * info
 
-
     print("\n".join([ "%s : %.5f" % (d,k/N) for d,k in simbolos ]))
     print()
 
 def callback(pkt):
     if pkt.haslayer(Ether):
-        dire = "BROADCAST" if pkt[Ether].dst=="ff:ff:ff:ff:ff:ff" else "UNICAST"
-        proto = pkt[Ether].type # El campo type del frame tiene el protocolo
-        s_i = (dire, proto) # Aca se define el simbolo de la fuente
+        dire = "BROADCAST" if pkt[Ether].dst == "ff:ff:ff:ff:ff:ff" else "UNICAST"
+        proto = pkt[Ether].type  # The 'type' field of the frame contains the protocol
+        s_i = (dire, proto)  # Define the symbol for the source
+
+        # Update the symbol count in the dictionary
         if s_i not in S1:
             S1[s_i] = 0.0
-            S1[s_i] += 1.0
+        S1[s_i] += 1.0
+
+try:
+    sniff(prn=callback)
+except KeyboardInterrupt:
+    # Handle keyboard interrupt (Ctrl+C)
     mostrar_fuente(S1)
-sniff(prn=callback)
+    print("Capture stopped by user.")
