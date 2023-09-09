1846314e5655b62a7532788a77d3b08b059bde1e
AlexONEX <a.schwartzmann@hotmail.com>
2023-09-09 16:20:58 -0300

    Terminado el catcher

--- packet_catcher.py
+++ packet_catcher.py
@@ -2,8 +2,13 @@
 
 from scapy.all import *
 import math 
+import pandas as pd
 
 S1 = {}
+pendingPackets = []
+baseFilename = "capture--"
+totalPackets = 0
+
 def mostrar_fuente(S):
     #S must be not empty
     N = sum(S.values())
@@ -12,32 +17,44 @@ def mostrar_fuente(S):
     information = {}
     entropy = 0.0
 
-    for s, p in simbolos:
-        probability = p / N 
-        info = -math.log(probabilidad, 2)
-        entropy += probability * info
+    for d,k in simbolos:
+        p = k/N
+        information[d] = -math.log2(p)
+        entropy += p * information[d]
 
-    print("Símbolos de la fuente y sus probabilidades:")
-    print("\n".join(["%s : %.5f" % (d, k / N) for d, k in simbolos]))
-    print("\nInformación de cada símbolo:")
-    print("\n".join(["%s : %.5f bits" % (s, info) for s, info in information.items()]))
-    print("\nEntropía de la fuente: %.5f bits" % entropy)
+    print("Entropy: %.5f" % entropy)
+    print("Information:")
+    print("\n".join([ "%s : %.5f" % (d,k/N) for d,k in simbolos ]))
+
+    df = pd.DataFrame.from_dict(S, orient='index')
+    df.to_csv('data.csv', header=False)
+    print(S.keys())
+    
     print()
 
 def callback(pkt):
+    global pendingPackets, totalPackets, S1
     if pkt.haslayer(Ether):
         dire = "BROADCAST" if pkt[Ether].dst == "ff:ff:ff:ff:ff:ff" else "UNICAST"
         proto = pkt[Ether].type  # The 'type' field of the frame contains the protocol
         s_i = (dire, proto)  # Define the symbol for the source
 
+        pendingPackets.append(pkt)
+        totalPackets += 1
+
         # Update the symbol count in the dictionary
         if s_i not in S1:
             S1[s_i] = 0.0
         S1[s_i] += 1.0
+        
+        if totalPackets % 1000 == 0:
+            print("Captured packets: ", totalPackets)
 
-try:
-    sniff(prn=callback)
-except KeyboardInterrupt:
-    # Handle keyboard interrupt (Ctrl+C)
-    mostrar_fuente(S1)
-    print("Capture stopped by user.")
+        if totalPackets >= 250000:
+            filename = baseFilename + str(totalPackets) + ".pcap"
+            wrpcap(filename, pendingPackets)
+            pendingPackets = [] 
+            mostrar_fuente(S1)
+            sys.exit(0)
+
+sniff(prn=callback)
