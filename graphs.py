8c4c95b5169a7729b5079dd747fbf4f0ca548492
Martin Santesteban <84803004+MartinSantesteban@users.noreply.github.com>
2023-09-19 00:56:24 -0300

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -47,26 +47,58 @@ def graphs_for_protocols(filepath, output_dir):
     plt.savefig(os.path.join(output_dir, 'scatter_plot.pdf'))
     plt.close()
 
-def graphs_for_information(filepath, output_dir, entropy):
-    with open(filepath, 'r') as file:
-        lines = file.readlines()
-
-    data = [line.strip().split(":") for line in lines[2:]]
-    symbols = [eval(d[0]) for d in data]
-    values = [float(d[1]) for d in data]
-    df = pd.DataFrame({'Simbolo': symbols, 'Informacion': values})
+#def graphs_for_information(filepath, output_dir, entropy):
+#    with open(filepath, 'r') as file:
+#        lines = file.readlines()
+#
+#    data = [line.strip().split(":") for line in lines[2:]]
+#    symbols = [eval(d[0]) for d in data]
+#    values = [float(d[1]) for d in data]
+#    df = pd.DataFrame({'Simbolo': symbols, 'Informacion': values})
     ##df.loc[df["Simbolo"].str[0] == "BROADCAST","Simbolo"] = ("B", df["Simbolo"].str[1])
     #df.loc[True, "Simbolo"] = 4
     #print(df)
-    df.plot(x='Simbolo', y='Informacion', kind='bar', legend=False)
+#    df.plot(x='Simbolo', y='Informacion', kind='bar', legend=False)
+#    plt.axhline(y=entropy, color='r', linestyle='--', label='Entropía')
+#    plt.ylabel('Información')
+#    plt.title('Información por Símbolo')
+#    plt.grid(axis='y')
+#    plt.legend()
+#    plt.tight_layout()
+#    plt.savefig(os.path.join(output_dir, 'information_bar_chart.pdf'))
+#    plt.close()
+
+def graphs_for_information(filepath, output_dir):
+    df = pd.read_csv(filepath)
+    protocol_map = {
+        2048: 'IP',
+        2054: 'ARP',
+        34525: 'IPv6', # Asegúrate de verificar este valor, es un ejemplo
+        34958: "PNAC",  #802.1X protocol—An IEEE standard for port-based network access control (PNAC) on wired and wireless access points. 
+        # 35130 que es???
+    }
+
+    type_map = {
+        "UNICAST": "U",
+        "BROADCAST": "B",
+    }
+
+    df['protocolo'] = df['protocolo'].replace(protocol_map)
+    df['tipo_destino'] = df['tipo_destino'].replace(type_map)
+
+    entropy =(df["probabilidad"] * df["informacion"]).sum()
+    df["simbolo"] = list(zip(df["tipo_destino"], df["protocolo"]))
+    fig = df.plot(x='simbolo', y='informacion' , kind='bar', legend=False)
+    #fig.set_xticklabels(df["probabilidad"])
     plt.axhline(y=entropy, color='r', linestyle='--', label='Entropía')
-    plt.ylabel('Información')
+    plt.ylabel('información')
     plt.title('Información por Símbolo')
     plt.grid(axis='y')
     plt.legend()
     plt.tight_layout()
     plt.savefig(os.path.join(output_dir, 'information_bar_chart.pdf'))
     plt.close()
+
 
 def graphs_for_destination(filepath, output_dir):
     # Cargar datos
@@ -84,7 +116,8 @@ def graphs_for_destination(filepath, output_dir):
     df_porcentajes = pd.DataFrame(data)
 
     # Crear un gráfico de barras para visualizar los porcentajes
-    df_porcentajes.plot(x='Tipo_Destino', y='Porcentaje', kind='bar', legend=False)
+    fig = df_porcentajes.plot(x='Tipo_Destino', y='Porcentaje', kind='bar', legend=False)
+    fig.bar_label(fig.containers[0], label_type='edge')
     plt.ylabel('Porcentaje')
     plt.title('Porcentaje de paquetes Unicast y Broadcast')
     plt.grid(axis='y')
@@ -123,9 +156,10 @@ for subdir in subdirs:
             graphs_for_protocols(filepath, output_dir)
             graphs_for_destination(filepath, output_dir)
             graphs_for_uni_broad_protocole(filepath, output_dir)
-        elif 'entropy+information' in csv_file:
-            with open(filepath, 'r') as file:
-                lines = file.readlines()
-            entropy = float(lines[0].split(":")[1].strip())
-            graphs_for_information(filepath, output_dir, entropy)
+            graphs_for_information(filepath, output_dir)
+        #elif 'entropy+information' in csv_file:
+        #    with open(filepath, 'r') as file:
+        #        lines = file.readlines()
+        #    entropy = float(lines[0].split(":")[1].strip())
+        #    graphs_for_information(filepath, output_dir, entropy)
 
