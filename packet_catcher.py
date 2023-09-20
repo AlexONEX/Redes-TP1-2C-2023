f8f20ae4ca22033e3e4bea13332fe225a5b33ab9
AlexONEX <a.schwartzmann@hotmail.com>
2023-09-20 20:33:31 -0300

    100 paquetes ARP source+destiny

--- packet_catcher.py
+++ packet_catcher.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 S1 = {}
 S2 = {}
+S3 = {}
 totalPackets = 0
 
 def mostrar_fuente(S):
@@ -42,6 +43,66 @@ def mostrar_fuente(S):
         f.write("\n".join([ "%s : %.5f" % (d,k/N) for d,k in simbolos ]))
         f.write("\n")
 
+
+def mostrar_fuente2(S):
+    #Recevie a dictionnary key = source IP, value = number of packets 
+    N = sum(S.values())
+    simbolos = sorted(S.items(), key=lambda x: -x[1])
+
+    information = {}
+    entropy = 0.0
+
+    for d,k in simbolos:
+        p = k/N
+        information[d] = (-1) * math.log2(p)
+        entropy += p * information[d]
+
+    df = pd.DataFrame(columns = ["IP", "cantidad", "probabilidad", "informacion"])
+    for k,v in simbolos:
+        df1 = pd.DataFrame({"IP" : [k], "cantidad" : [v], "probabilidad" : [v/N], "informacion" : [information[k]]})
+        df = pd.concat([df,df1], axis = 0, ignore_index = True)
+    df.to_csv('all_data.csv')
+    print(df)
+
+    #write entropy and probabilities to Filter
+    with open('entropy+information.csv', 'a') as f:
+        f.write("Entropy: %.5f" % entropy)
+        f.write("\n")
+        f.write("Information:")
+        f.write("\n")
+        f.write("\n".join([ "%s : %.5f" % (d,k/N) for d,k in simbolos ]))
+        f.write("\n")
+
+def mostrar_fuente3(S):
+    #Recevie a dictionnary key = source IP, value = number of packets 
+    N = sum(S.values())
+    simbolos = sorted(S.items(), key=lambda x: -x[1])
+
+    information = {}
+    entropy = 0.0
+
+    for d,k in simbolos:
+        p = k/N
+        information[d] = (-1) * math.log2(p)
+        entropy += p * information[d]
+
+    df = pd.DataFrame(columns = ["IP", "cantidad", "probabilidad", "informacion"])
+    for k,v in simbolos:
+        df1 = pd.DataFrame({"IP" : [k], "cantidad" : [v], "probabilidad" : [v/N], "informacion" : [information[k]]})
+        df = pd.concat([df,df1], axis = 0, ignore_index = True)
+    df.to_csv('all_data2.csv')
+    print(df)
+
+    #write entropy and probabilities to Filter
+    with open('entropy+information2.csv', 'a') as f:
+        f.write("Entropy: %.5f" % entropy)
+        f.write("\n")
+        f.write("Information:")
+        f.write("\n")
+        f.write("\n".join([ "%s : %.5f" % (d,k/N) for d,k in simbolos ]))
+        f.write("\n")
+    
+
 def callback(pkt):
     global totalPackets, S1, S2
     #if pkt.haslayer(Ether):
@@ -57,18 +118,26 @@ def callback(pkt):
     # Filter by ARP and then store the source IP 
     if pkt.haslayer(ARP):
         s_i = pkt[ARP].psrc
+        s_j = pkt[ARP].pdst
         if s_i not in S2:
             S2[s_i] = 0.0
+        if s_j not in S3:
+            S3[s_j] = 0.0
         S2[s_i] += 1.0
+        S3[s_j] += 1.0
         totalPackets += 1
         print(s_i)
-
     
-    if totalPackets >= 12500:
+    if totalPackets >= 1000:
        # mostrar_fuente(S1, 'all_data.csv')
-        mostrar_fuente(S2, 'S2_data.csv')
+        mostrar_fuente2(S2)
+        mostrar_fuente3(S3)
         sys.exit(0)
-    else:
-        print("Packet %d" % totalPackets)
 
-sniff(prn=callback)
+
+try:
+    sniff(prn=callback)
+except KeyboardInterrupt:
+    mostrar_fuente2(S2)
+    mostrar_fuente3(S3)
+    sys.exit(0)
